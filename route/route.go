1965f29bfa0eb1516d0ad288f9c9544d8a3fc598
Solomon Okwa <dikaeinstein@github.com>
2018-12-29 17:59:54 +0100

    namespace routes into /api

--- route/route.go
+++ route/route.go
@@ -15,15 +15,16 @@ func init() {
 
 func routes() *mux.Router {
 	router := mux.NewRouter()
+	api := router.PathPrefix("/api").Subrouter()
 
-	router.Path("/api").HandlerFunc(controller.Welcome).Methods("GET")
-	router.Path("/api/user/new").HandlerFunc(controller.CreateAccount).Methods("POST")
-	router.Path("/api/user/login").HandlerFunc(controller.Authenticate).Methods("POST")
-	router.Path("/api/contacts/new").HandlerFunc(controller.CreateContact).Methods("POST")
-	router.Path("/api/me/contacts").HandlerFunc(controller.GetContactsFor).Methods("GET")
-	router.Path("/*").HandlerFunc(controller.NotFound).Methods()
+	api.Path("").HandlerFunc(controller.Welcome).Methods("GET")
+	api.Path("/user/new").HandlerFunc(controller.CreateAccount).Methods("POST")
+	api.Path("/user/login").HandlerFunc(controller.Authenticate).Methods("POST")
+	api.Path("/contacts/new").HandlerFunc(controller.CreateContact).Methods("POST")
+	api.Path("/me/contacts").HandlerFunc(controller.GetContactsFor).Methods("GET")
+	api.Path("/").HandlerFunc(controller.NotFound)
 
-	router.Use(app.JwtAuthentication) // Attach JWT auth middleware
+	api.Use(app.JwtAuthentication) // Attach JWT auth middleware
 
 	return router
 }
