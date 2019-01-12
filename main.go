bef5bb7dfbe305724ef1d9a704c0c22778b03110
Solomon Okwa <dikaeinstein@github.com>
2019-01-12 14:44:22 +0100

    implement graceful shutdown of server

--- main.go
+++ main.go
@@ -1,16 +1,23 @@
 package main
 
 import (
+	"context"
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"time"
 
 	"github.com/dikaeinstein/go-rest-api/model"
 	"github.com/dikaeinstein/go-rest-api/route"
 )
 
+var wait = flag.Duration("graceful-timeout", time.Second*15, "the duration for which the server gracefully wait for existing connections to finish - e.g. 15s or 1m")
+
 func main() {
+	flag.Parse()
 	d := model.GetDB()
 	defer d.Close() // Close db connection
 
@@ -19,10 +26,43 @@ func main() {
 		port = "4000"
 	}
 
-	fmt.Println("localhost:" + port)
-
-	err := http.ListenAndServe(":"+port, route.Router)
-	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
+	addr := "localhost:" + port
+	srv := http.Server{
+		Addr:    addr,
+		Handler: route.Router,
+		// Good practice to set timeouts to avoid Slowloris attacks.
+		WriteTimeout: time.Second * 15,
+		ReadTimeout:  time.Second * 15,
+		IdleTimeout:  time.Second * 60,
 	}
+
+	// Run our server in a goroutine so that it doesn't block.
+	go func() {
+		fmt.Println("localhost:" + port)
+		err := srv.ListenAndServe()
+		if err != nil {
+			log.Println("ListenAndServe: ", err)
+		}
+	}()
+
+	c := make(chan os.Signal, 1)
+	// We'll accept graceful shutdowns when quit via SIGINT (Ctrl+C)
+	// SIGKILL, SIGQUIT or SIGTERM (Ctrl+/) will not be caught.
+	signal.Notify(c, os.Interrupt)
+
+	// Block until we receive signal
+	<-c
+
+	// Create a deadline to wait for
+	ctx, cancel := context.WithTimeout(context.Background(), *wait)
+	defer cancel()
+	// Doesn't block if no connections, but will otherwise wait
+	// until the timeout deadline.
+	srv.Shutdown(ctx)
+	// Optionally, you could run srv.Shutdown in a goroutine and block on
+	// <-ctx.Done() if your application should wait for other services
+	// to finalize based on context cancellation.
+	fmt.Println()
+	log.Println("shutting down...")
+	os.Exit(0)
 }
