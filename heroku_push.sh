45fd256e94e840c955cdba6c7a0d9d256b2e201e
Solomon Okwa <dikaeinstein@github.com>
2019-02-01 20:46:42 +0100

    make image tag dynamic

--- heroku_push.sh
+++ heroku_push.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker login --username=_ --password=$HEROKU_API_KEY registry.heroku.com
-docker push dikaeinstein/go-rest-api
+docker push registry.heroku.com/dikaeinstein-go-rest-api:${HEROKU_LABEL}/web
