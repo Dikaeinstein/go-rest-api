21b04a20d00c665965a199d426f3d1768b156167
Solomon Okwa <dikaeinstein@github.com>
2019-02-01 20:58:08 +0100

    remove HEROKU_LABEL

--- heroku_push.sh
+++ heroku_push.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker login --username=_ --password=$HEROKU_API_KEY registry.heroku.com
-docker push registry.heroku.com/dikaeinstein-go-rest-api:${HEROKU_LABEL}/web
+docker push registry.heroku.com/dikaeinstein-go-rest-api/web
