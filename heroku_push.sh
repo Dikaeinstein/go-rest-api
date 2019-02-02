dc135e971909b2d7d9727f39268db72e0ed3fe73
Solomon Okwa <dikaeinstein@github.com>
2019-02-02 16:07:09 +0100

    add heroku container release to deployment script

--- heroku_push.sh
+++ heroku_push.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 docker login --username=_ --password=$HEROKU_API_KEY registry.heroku.com
 docker push registry.heroku.com/dikaeinstein-go-rest-api/web
+
+# Release container via API
+curl -n -X PATCH https://api.heroku.com/apps/dikaeinstein-go-rest-api/formation \
+  -d '{
+  "updates": [
+    {
+      "type": "web",
+      "docker_image": "$WEB_DOCKER_IMAGE_ID"
+    }
+  ]
+}' \
+  -H "Content-Type: application/json" \
+  -H "Accept: application/vnd.heroku+json; version=3.docker-releases"
