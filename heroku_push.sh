bd92e50edd0ed318bb89a4514e142846621f51d9
Solomon Okwa <dikaeinstein@github.com>
2019-02-03 20:11:44 +0100

    tag docker images

--- heroku_push.sh
+++ heroku_push.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 docker login --username=_ --password=$HEROKU_API_KEY registry.heroku.com
+docker tag dikaeinstein-go-rest-api:$DOCKER_IMAGE_TAG registry.heroku.com/dikaeinstein-go-rest-api/web
 docker push registry.heroku.com/dikaeinstein-go-rest-api/web
 
 # Release container via heroku CLI
