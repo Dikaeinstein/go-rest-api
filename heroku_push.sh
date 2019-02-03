1717b90f1e79ad030bbca72bc84a497795d62ac5
Solomon Okwa <dikaeinstein@github.com>
2019-02-03 01:54:29 +0100

    release dockerized app

--- heroku_push.sh
+++ heroku_push.sh
@@ -2,17 +2,5 @@
 docker login --username=_ --password=$HEROKU_API_KEY registry.heroku.com
 docker push registry.heroku.com/dikaeinstein-go-rest-api/web
 
-# Release container via CLI
-# curl -n -X PATCH https://api.heroku.com/apps/dikaeinstein-go-rest-api/formation \
-#   -d '{
-#   "updates": [
-#     {
-#       "type": "web",
-#       "docker_image": "$WEB_DOCKER_IMAGE_ID"
-#     }
-#   ]
-# }' \
-#   -H "Content-Type: application/json" \
-#   -H "Authorization: Bearer $HEROKU_API_KEY" \
-#   -H "Accept: application/vnd.heroku+json; version=3.docker-releases"
-heroku container:release web
+# Release container via heroku CLI
+heroku container:release web -a dikaeinstein-go-rest-api
